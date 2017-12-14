c5772526266d7a1c12a28f7a4706fe32b75c346a
JackNoordhuis <daswaggamer@gmail.com>
2017-12-14 22:24:30 +1100

    Update copyright headers

--- src/jacknoordhuis/battles/battle/types/TeamBattle.php
+++ src/jacknoordhuis/battles/battle/types/TeamBattle.php
@@ -3,7 +3,7 @@
 /*
  * Battles plugin for PocketMine-MP
  *
- * Copyright (C) 2017 JackNoordhuis
+ * Copyright (C) 2017-2018 JackNoordhuis
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
